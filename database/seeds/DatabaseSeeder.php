f26baa4def62a50bd529d715e8a02c75fa0b2ea7
muklasr <muclasrachmanto@gmail.com>
2019-11-19 09:27:25 +0700

    add seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Pelanggan;
+use App\Kategori;
+use App\Produk;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,5 +15,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        factory(Pelanggan::class, 100)->create();
+        factory(Kategori::class, 5)->create();
+        factory(Produk::class, 50)->create();
     }
 }
