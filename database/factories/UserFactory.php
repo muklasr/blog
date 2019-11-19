f38e8ea23c9e742926e60f69dfd3f18139cc1c72
muklasr <muclasrachmanto@gmail.com>
2019-11-19 09:27:12 +0700

    add factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,6 +2,9 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 use App\User;
+use App\Pelanggan;
+use App\Kategori;
+use App\Produk;
 use Faker\Generator as Faker;
 use Illuminate\Support\Str;
 
@@ -23,5 +26,26 @@ $factory->define(User::class, function (Faker $faker) {
         'email_verified_at' => now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
+    ];
+});
+
+$factory->define(Pelanggan::class, function (Faker $faker) {
+    return [
+        'nama' => $faker->name,
+        'email' => $faker->unique()->safeEmail,
+        'alamat' => $faker->address,
+    ];
+});
+
+$factory->define(Produk::class, function (Faker $faker) {
+    return [
+        'name' => $faker->name,
+        'stok' => $faker->numberBetween(0,100),
+    ];
+});
+
+$factory->define(Kategori::class, function (Faker $faker) {
+    return [
+        'name' => $faker->name,
     ];
 });
